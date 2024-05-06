26701bd0c6a0e5a96ff41e8a9aede50d6507113c
Arbind Kumar Mahato <you@example.com>
2024-05-07 00:52:58 +0545

    upldate

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,35 @@ import "./App.css";
 function App() {
   const [name, setname] = useState<string>("");
   const [message, setMessage] = useState<string>("");
+  const [code, setCode] = useState<number>(0);
+  let [reply, setreply] = useState<string>("");
   const onCheck = async () => {
     const response = await fetch("https://guess-93sv.onrender.com/" + name);
     const data = await response.json();
     setMessage(data.message);
+    setCode(data.code);
+  };
+  const sendReply = async () => {
+    const response = await fetch("https://guess-93sv.onrender.com/reply", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: reply,
+      }),
+    });
+    if (response.ok) {
+      alert("sent successfully!");
+    }
   };
   return (
     <>
       <div>
         {!message ? (
           <>
-            Hey, Enter the smarttest person from our class, his/her first name
+            Hey, maine nepali class main kuchh toh ek nepali word likha jise
+            main bahar hogaya, usko likho nepali main.
             <input
               type="text"
               id="name"
@@ -26,6 +44,18 @@ function App() {
         ) : (
           <div>
             Your Message: {message} or try again if failed
+            {code === 444 && (
+              <div>
+                your reply here:
+                <input
+                  type="text"
+                  id="reply"
+                  onChange={(event) => setreply(event.target.value)}
+                  value={reply}
+                />
+                <button onClick={sendReply}>SEND!</button>
+              </div>
+            )}
             <button onClick={() => setMessage("")}>Try again</button>
           </div>
         )}
